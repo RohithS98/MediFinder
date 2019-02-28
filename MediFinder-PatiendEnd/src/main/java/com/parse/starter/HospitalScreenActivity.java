a15d7671f220e0b5b539af20c684ccf21ad4017c
RohithS98 <ee16b025@iittp.ac.in>
2019-02-28 13:36:12 +0530

    Merged hospital and patient

---
+++ MediFinder-PatiendEnd/src/main/java/com/parse/starter/HospitalScreenActivity.java
@@ -0,0 +1,34 @@
+package com.parse.starter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class HospitalScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital_screen);
+    }
+
+    public void updateInfo(View view){
+        Intent intent = new Intent(HospitalScreenActivity.this, SignUp2Activity.class);
+        startActivity(intent);
+    }
+
+    public void logOut(View view) {
+        ParseUser.logOutInBackground(new LogOutCallback() {
+            @Override
+            public void done(ParseException e) {
+                Intent intent = new Intent(HospitalScreenActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
